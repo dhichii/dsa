7c0bd5d17a374a8a645842e78fad5ebf9b45d31d
Adhicitta Masran <adhicittamasran@gmail.com>
2024-06-11 19:15:24 +0800

    solve lru cache

--- node/src/lru-cache.ts
+++ node/src/lru-cache.ts
@@ -24,13 +24,72 @@ export function createLRUCacheProvider<T>({
   ttl,
   itemLimit,
 }: LRUCacheProviderOptions): LRUCacheProvider<T> {
+  const cacheKey: string[] = []
+  const cache = new Map()
+  const updateAccessedCache = (key: string) => {
+    const data = cache.get(key)
+    if (data) {
+      // delete old data
+      cacheKey.splice(cacheKey.indexOf(key), 1)
+      cache.delete(key)
+
+      // add new data
+      data.time = new Date().getTime() + ttl
+      cache.set(key, data)
+      cacheKey.unshift(key)
+    }
+  }
+  const isExpired = (key: string) => {
+    const data = cache.get(key)
+    if (!data) {
+      return
+    }
+
+    const currTimestamp = new Date()
+    if (currTimestamp >= data.time) {
+      cacheKey.splice(cacheKey.indexOf(key), 1)
+      cache.delete(key)
+
+      return true
+    }
+  }
+
   return {
     has: (key: string) => {
+      if (cache.has(key)) {
+        if (isExpired(key)) {
+          return false
+        }
+
+        updateAccessedCache(key)
+
+        return true
+      }
+
       return false
     },
     get: (key: string) => {
-      return undefined
+      isExpired(key)
+      updateAccessedCache(key)
+      const res = cache.get(key)?.value
+
+      return res
     },
-    set: (key: string, value: T) => {},
+    set: (key: string, value: T) => {
+      const len = cacheKey.length
+      if (len === itemLimit) {
+        const last = cacheKey[len-1]
+        cacheKey.pop()
+        cache.delete(last)
+      }
+
+      cacheKey.unshift(key)
+
+      const data = {
+        value,
+        time: new Date().getTime() + ttl,
+      }
+      cache.set(key, data)
+    },
   }
 }
